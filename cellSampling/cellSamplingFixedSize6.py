b3f059a5df44b95d34d0db5e47134f632369c18e
liyu10000 <yuli00986081@gmail.com>
2018-09-05 14:59:15 +0800

    update

---
+++ cellSampling/cellSamplingFixedSize6.py
@@ -0,0 +1,94 @@
+# coding=utf-8
+# crop marked region, place labeled box in a fixed sized image, at specified position
+
+import os
+import openslide
+import scipy.misc
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+
+colors = {"#aa0000": "HSIL", "#aa007f": "ASCH", "#005500": "LSIL", "#00557f": "ASCUS", 
+          "#0055ff": "SCC", "#aa557f": "ADC", "#aa55ff": "EC", "#ff5500": "AGC1", 
+          "#ff557f": "AGC2", "#ff55ff": "AGC3", "#00aa00": "FUNGI", "#00aa7f": "TRI", 
+          "#00aaff": "CC", "#55aa00": "ACTINO", "#55aa7f": "VIRUS",
+          "#000000": "MC", "#aa00ff": "SC", "#ff0000": "RC", "#aa5500": "GEC"}
+
+
+def scan_files(directory, prefix=None, postfix=None):
+    files_list = []
+    for root, sub_dirs, files in os.walk(directory):
+        for special_file in files:
+            if postfix:
+                if special_file.endswith(postfix):
+                    files_list.append(os.path.join(root, special_file))
+            elif prefix:
+                if special_file.startswith(prefix):
+                    files_list.append(os.path.join(root, special_file))
+            else:
+                files_list.append(os.path.join(root, special_file))
+    return files_list
+
+
+def get_xy(box, size, position):
+    """
+    cut image out of label box.
+    note: label box should be within image
+    :params box: (xmin, ymin, xmax, ymax)
+    :params size: image size to cut out
+    :params position: (x_percent, y_percent), the center of label box relative to image.
+                      if need to put label box in center of image, set position to (0.5, 0.5)
+    :return: the upper-left coordinates (x, y) of image. if anything wrong happens, return (-1, -1)
+    """
+    box_center_x = (box[0]+box[2])/2
+    box_center_y = (box[1]+box[3])/2
+    x = box_center_x - size*position[0]
+    y = box_center_y - size*position[1]
+    if x > box[0] or y > box[1] or x+size < box[2] or y+size < box[3]:
+        return (-1, -1)
+    return (int(x), int(y))
+
+
+def cut_cells(xml_file, save_path, size):
+    # get basename, without extension
+    basename = os.path.splitext(os.path.basename(xml_file))[0]
+    # open .tif file
+    tif_file = os.path.join(os.path.dirname(xml_file), basename+".tif")
+    count = 0
+    slide = openslide.OpenSlide(tif_file)
+    # open .xml file
+    DOMTree = xml.dom.minidom.parse(xml_file)
+    collection = DOMTree.documentElement
+    annotations = collection.getElementsByTagName("Annotation")
+    for annotation in annotations:
+        if annotation.getAttribute("Color") in colors:
+            coordinates = annotation.getElementsByTagName("Coordinate")
+            # read (x, y) coordinates
+            x_coords = [float(coordinate.getAttribute("X")) for coordinate in coordinates]
+            y_coords = [float(coordinate.getAttribute("Y")) for coordinate in coordinates]
+            if len(x_coords) < 3:
+                continue
+            # get the (x, y) coordinates for read_region()
+            x, y = get_xy((min(x_coords), min(y_coords), max(x_coords), max(y_coords)), size, (0.5, 0.5))
+            if x == -1:
+                continue
+            save_path_i = os.path.join(save_path, basename, colors[annotation.getAttribute("Color")])
+            os.makedirs(save_path_i, exist_ok=True)
+            cell = slide.read_region((x, y), 0, (size, size)).convert("RGB")
+            cell.save(os.path.join(save_path_i, "{}_{}_{}.jpg".format(basename, x, y)))
+            count += 1
+    slide.close()
+    print(basename + ": number of cells " + str(count))
+
+
+def batch_process(path_in, path_out, size):
+    files_list = scan_files(path_in, postfix=".xml")
+    for f in files_list:
+        cut_cells(f, path_out, size)
+
+
+if __name__ == "__main__":
+    path_in = "/home/sakulaki/yolo-yuli/one_stop_test/tif_xml"
+    path_out = "/home/sakulaki/yolo-yuli/one_stop_test/tif_xml_cells"
+    batch_process(path_in, path_out, 512)    
+
--- data_preparation/dicom_reader.py
+++ data_preparation/dicom_reader.py
@@ -1,14 +1,79 @@
 import os
+import cv2
+import png
 import pydicom
+import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
-from utils.scan_files import scan_files
+from PIL import Image, ImageEnhance
 
-dcms = scan_files("C:\\Users\\tsimage\\Desktop\\lung\\李京", postfix=".dcm")
-save_path = "../res/temp/test"
+
+def scan_files(directory, prefix=None, postfix=None):
+    files_list = []
+    for root, sub_dirs, files in os.walk(directory):
+        for special_file in files:
+            if postfix:
+                if special_file.endswith(postfix):
+                    files_list.append(os.path.join(root, special_file))
+            elif prefix:
+                if special_file.startswith(prefix):
+                    files_list.append(os.path.join(root, special_file))
+            else:
+                files_list.append(os.path.join(root, special_file))
+    return files_list
+
+
+dcms = scan_files("/media/tsimage001/TSIMAGE/CT/CT_a784d07a2d539b8a6d9448a3892a5d01/118ba86eedb66f8d2b0b30c83d3762d3", postfix=".dcm")
+save_path = "/media/tsimage001/TSIMAGE/CT-png/CT_a784d07a2d539b8a6d9448a3892a5d01/118ba86eedb66f8d2b0b30c83d3762d3"
 if not os.path.exists(save_path):
-    os.mkdir(save_path)
+    os.makedirs(save_path)
+
+count = 0
 for dcm in dcms:
-    name = os.path.splitext(os.path.basename(dcm))[0]
+    # name = os.path.splitext(os.path.basename(dcm))[0]
+    # img_name = os.path.join(save_path, name+".png")
+    index = os.path.splitext(os.path.basename(dcm))[0].rsplit("_",1)[1][-3:]
+    img_name = os.path.join(save_path, "img_{}_i.png".format(index))
+
     ds = pydicom.dcmread(dcm)
-    plt.imsave(os.path.join(save_path, name+".jpg"), ds.pixel_array)
-    print(name + " saved")
+    shape = ds.pixel_array.shape
+    # Convert to float to avoid overflow or underflow losses.
+    img = ds.pixel_array.astype(float)
+
+    # if count == 0:
+    #     img = (img-img.min())/(img.max()-img.min())*255.0
+    #     # img = np.clip(np.int8(img), 50, 255)
+    #     plt.subplot(121)
+    #     sns.distplot(img.flatten())
+    #     plt.subplot(122)
+    #     plt.imshow(img, cmap=plt.cm.gray)
+    #     plt.show()
+    #     count += 1
+
+    # brightness = 30
+    # contrast = 10
+    # img = img * (contrast/127+1) - contrast + brightness
+    img = (img-img.min())/(img.max()-img.min())*255.0
+    # img = np.clip(img, 0, 255)
+    img = np.uint8(img)
+
+    # img = np.int8(img)
+    # img = [[max(pixel-25, 0) if pixel < 200 else min(pixel+25, 255) for pixel in row] for row in img]
+
+    # # Rescaling grey scale between 0-255
+    # img = (np.maximum(img,0) / img.max()) * 255.0
+    # # Convert to uint
+    # img = np.uint8(img)
+
+    # # enhance image
+    # ImageEnhance.Brightness(Image.fromarray(img)).enhance(7.5).save(img_name)
+
+    # # power law transformation (gamma correction)
+    # img = cv2.pow(img, 0.6)
+
+    # Write the PNG file
+    with open(img_name, 'wb') as png_file:
+        w = png.Writer(shape[1], shape[0], greyscale=True)
+        w.write(png_file, img)
+
+    # plt.imsave(img_name, ds.pixel_array, cmap=plt.cm.gray)
