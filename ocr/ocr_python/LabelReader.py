6729926637393e847a7878d285310cad3101b663
liyu10000 <yuli00986081@gmail.com>
2018-09-10 10:33:42 +0800

    restructure and modified window dll loading

--- ocr/ocr_python/LabelReader.py
+++ ocr/ocr_python/LabelReader.py
@@ -1,8 +1,6 @@
 import sys
 from ctypes import *
 from PIL import Image
-import numpy as np
-from io import BytesIO
 
 
 class LabelReader:
@@ -20,33 +18,33 @@ class LabelReader:
 
     def read_label(self, wsi_name):
         if self.os == "linux":
-            # from lib.tslide.tslide import TSlide
-            # slide = TSlide(wsi_name)
-            # # print(slide.associated_images.items())
-            # image = slide.associated_images["label"].convert("RGB")  # RGB Image instance
-            # # w, h = image.size
-            # # image.crop((0, 0, w, h//2)).save("./res/half.jpg")
-            # # image.save("./res/label.jpg")
-            # slide.close()
-            # return image
-            lib = cdll.LoadLibrary('tslide/libkfbslide.so')
-            
+            from tslide.tslide import TSlide
+            slide = TSlide(wsi_name)
+            # print(slide.associated_images.items())
+            image = slide.associated_images["label"].convert("RGB")  # RGB Image instance
+            # w, h = image.size
+            # image.crop((0, 0, w, h//2)).save("./res/half.jpg")
+            # image.save("./res/label.jpg")
+            slide.close()
+            return image
         else:
-            lib = WinDLL("./lib/ImageOperationLib.dll")
+            _lib = WinDLL("./lib/ImageOperationLib.dll")
 
-            lib.GetLableInfoPathFunc.argtypes = [c_char_p, POINTER(POINTER(c_ubyte)), POINTER(c_int), POINTER(c_int), POINTER(c_int)]
-            lib.GetLableInfoPathFunc.restype = c_int
+            _lib.GetLableInfoPathFunc.argtypes = [c_char_p, POINTER(POINTER(c_ubyte)), POINTER(c_int), POINTER(c_int), POINTER(c_int)]
+            _lib.GetLableInfoPathFunc.restype = c_int
             path = c_char_p(wsi_name.encode("utf-8"))
             imageData = POINTER(c_ubyte)()
             length = c_int()
             width = c_int()
             height = c_int()
             res = c_int()
-            res = lib.GetLableInfoPathFunc(path, byref(imageData), byref(length), byref(width), byref(height))
+            res = _lib.GetLableInfoPathFunc(path, byref(imageData), byref(length), byref(width), byref(height))
             if res:
                 # with open("./res/label_win.jpg", "wb") as f:
                 #     f.write(string_at(imageData, length.value))
+                import numpy as np
                 narray = np.ctypeslib.as_array(imageData, shape=(length.value,))
+                from io import BytesIO
                 buf = BytesIO(narray)
                 # Image.open(buf).save("./res/label_win.jpg")
                 return Image.open(buf)
