8c2e5ad2114c267a5615924e85f81dc2f347e6bc
liyu10000 <yuli00986081@gmail.com>
2018-09-18 09:40:10 +0800

    update with multiprocessing

--- darknet/cell_slicing_tif.py
+++ darknet/cell_slicing_tif.py
@@ -2,15 +2,17 @@
 import os
 import openslide
 import scipy.misc
-from xml.dom.minidom import parse
 import xml.dom.minidom
+
+from multiprocessing import cpu_count
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tslide.tslide import TSlide
 
 from gen_xml import Xml
 
 
 def scan_files(directory, prefix=None, postfix=None):
     files_list = []
-
     for root, sub_dirs, files in os.walk(directory):
         for special_file in files:
             if postfix:
@@ -21,15 +23,14 @@ def scan_files(directory, prefix=None, postfix=None):
                     files_list.append(os.path.join(root, special_file))
             else:
                 files_list.append(os.path.join(root, special_file))
-
     return files_list
 
 
-# colors = {"#000000": "MC", "#aa0000": "HSIL", "#aa007f": "ASCH", "#aa00ff": "SC", "#ff0000": "RC",
+# classes = {"#000000": "MC", "#aa0000": "HSIL", "#aa007f": "ASCH", "#aa00ff": "SC", "#ff0000": "RC",
 #           "#005500": "LSIL", "#00557f": "ASCUS", "#0055ff": "SCC", "#aa5500": "GEC", "#aa557f": "ADC",
 #           "#aa55ff": "EC", "#ff5500": "AGC1", "#ff557f": "AGC2", "#ff55ff": "AGC3", "#00aa00": "FUNGI",
 #           "#00aa7f": "TRI", "#00aaff": "CC", "#55aa00": "ACTINO", "#55aa7f": "VIRUS"}
-colors = {"#aa0000": "HSIL", "#aa007f": "ASCH",
+classes = {"#aa0000": "HSIL", "#aa007f": "ASCH",
           "#005500": "LSIL", "#00557f": "ASCUS", "#0055ff": "SCC", "#aa557f": "ADC",
           "#aa55ff": "EC", "#ff5500": "AGC1", "#ff557f": "AGC2", "#ff55ff": "AGC3", "#00aa00": "FUNGI",
           "#00aa7f": "TRI", "#00aaff": "CC", "#55aa00": "ACTINO", "#55aa7f": "VIRUS"}
@@ -47,7 +48,7 @@ def get_labels(xml_file):
     labels = {}
     i = 0
     for annotation in annotations:
-        if annotation.getAttribute("Color") in colors:
+        if annotation.getAttribute("Color") in classes:
             coordinates = annotation.getElementsByTagName("Coordinate")
             x_coords = []
             y_coords = []
@@ -58,7 +59,7 @@ def get_labels(xml_file):
             x_max = int(max(x_coords))
             y_min = int(min(y_coords))
             y_max = int(max(y_coords))
-            labels[i] = (x_min, x_max, y_min, y_max, colors[annotation.getAttribute("Color")])
+            labels[i] = (x_min, x_max, y_min, y_max, classes[annotation.getAttribute("Color")])
             i += 1
     return labels
 
@@ -75,14 +76,17 @@ def get_windows(labels, size_x, size_y, size):
     while x + size <= size_x:
         while y + size <= size_y:
             for i, label in labels.items():
-                if (x <= label[0] and label[1] <= x+size and y <= label[2] and label[3] <= y+size) or ((label[0] <= x and x+size <= label[1]) and (label[2] <= y and y+size <= label[3])) or ((label[0] <= x and x+size <= label[1]) and (y <= label[2] and label[3] <= y+size)) or ((x <= label[0] and label[1] <= x+size) and (label[2] <= y and y+size <= label[3])):
+                if (x <= label[0] and label[1] <= x+size and y <= label[2] and label[3] <= y+size) or 
+                   ((label[0] <= x and x+size <= label[1]) and (label[2] <= y and y+size <= label[3])) or 
+                   ((label[0] <= x and x+size <= label[1]) and (y <= label[2] and label[3] <= y+size)) or 
+                   ((x <= label[0] and label[1] <= x+size) and (label[2] <= y and y+size <= label[3])):
                     if (x, y) in points_xy:
                         points_xy[(x, y)].append(i)
                     else:
                         points_xy[(x, y)] = [i,]
-            y += 100
+            y += 200
         y = 0
-        x += 100
+        x += 200
     
     # remove duplicates
     points_xy_new = {}
@@ -103,49 +107,67 @@ def get_windows(labels, size_x, size_y, size):
     return points_xy_new
 
     
-def cell_sampling(files_list, save_path, size):
-    for xml_file in files_list:
-        labels = get_labels(xml_file)
-        print(labels)
-        
-        filename = os.path.splitext(xml_file)[0]
-        tif_file = filename + ".tif"
-        slide = openslide.OpenSlide(tif_file)
-        
-        [size_x, size_y] = slide.dimensions
-        points_xy = get_windows(labels, size_x, size_y, size)
-        print(points_xy)
-        
-        # generate jpg files      
-        for (x, y) in points_xy:
-            cell = slide.read_region((x, y), 0, (size, size))
-            cell = cell.convert("RGB")
-            scipy.misc.imsave(save_path + "/" + os.path.basename(filename) + "_" + str(x) + "_" + str(y) + ".jpg", cell)
+def cell_sampling(xml_file, save_path, size):
+    labels = get_labels(xml_file)
+    # print(labels)
+    
+    filename = os.path.splitext(xml_file)[0]
+    if (not os.path.isfile(filename+".tif")) and (not os.path.isfile(filename+".kfb")):
+        print(filename + " doesn't exist")
+        return
+    try:
+        slide = openslide.OpenSlide(filename+".tif")
+    except:
+        slide = TSlide(filename+".kfb")
+    
+    size_x, size_y = slide.dimensions
+    points_xy = get_windows(labels, size_x, size_y, size)
+    # print(points_xy)
+    
+    # generate jpg files      
+    for (x, y) in points_xy:
+        cell = slide.read_region((x, y), 0, (size, size)).convert("RGB")
+        cell.save(save_path + "/" + os.path.basename(filename) + "_" + str(x) + "_" + str(y) + ".jpg")
 
-        slide.close()
+    slide.close()
+    
+    # generate xml files
+    new_xmls = Xml(os.path.basename(filename), save_path, points_xy, labels, size)
+    new_xmls.gen_xml()
+
+    print("processed ", xml_file)
+
+
+def main(path_in, path_out, size):
+    sub_dirs = os.listdir(path_in)
+    for sub_dir in sub_dirs:
+        file_path_i = os.path.join(path_in, sub_dir)
+        xml_files = scan_files(file_path_i, postfix=".xml")
+        save_path_i = os.path.join(path_out, sub_dir)
+        os.makedirs(save_path_i, exist_ok=True)
+
+        executor = ProcessPoolExecutor(max_workers=cpu_count() - 4)
+        tasks = []
+        for xml_file in xml_files:
+            tasks.append(executor.submit(cell_sampling, xml_file, save_path_i, size))
         
-        # generate xml files
-        new_xmls = Xml(os.path.basename(filename), save_path, points_xy, labels, size)
-        new_xmls.gen_xml()
+        job_count = len(tasks)
+        for future in as_completed(tasks):
+            # result = future.result()  # get the returning result from calling fuction
+            job_count -= 1
+            # print("One Job Done, last Job Count: %s" % (job_count))
+
 
 
 if __name__ == "__main__":
     file_path = "/media/tsimage/Elements/data"
     save_path = "/media/tsimage/Elements/data/tct_data608_0716"
-    #classes = ("01_ASCUS", "02_LSIL", "03_ASCH", "05_SCC", "06_AGC1", "07_AGC2", "08_ADC", "09_EC", "10_FUNGI", "11_TRI", "12_CC", "13_ACTINO", "14_VIRUS")
-    classes = ("04_HSIL",)
-    for class_i in classes:
-        file_path_i = os.path.join(file_path, class_i)
-        files_list = scan_files(file_path_i, postfix=".xml")
-        save_path_i = os.path.join(save_path, class_i)
-        if not os.path.exists(save_path_i):
-            os.makedirs(save_path_i)
-        cell_sampling(files_list, save_path_i, 608)
+    main(file_path, save_path, 608)
 
     # # single directory cell sampling
     # file_path = "/media/tsimage/Elements/data/01_ASCUS"
-    # files_list = scan_files(file_path, postfix=".xml")
+    # xml_files = scan_files(file_path, postfix=".xml")
     # save_path = "/media/tsimage/Elements/data/tct_data608_0716"
     # if not os.path.exists(save_path):
     #     os.makedirs(save_path)
-    # cell_sampling(files_list, save_path, 608)
+    # cell_sampling(xml_files, save_path, 608)
