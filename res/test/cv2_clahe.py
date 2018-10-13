9ca85d5acc442f77d56ddb058d6f13dde5e3389d
liyu10000 <yuli00986081@gmail.com>
2018-10-13 14:47:40 +0800

    added clahe example code

---
+++ res/test/cv2_clahe.py
@@ -0,0 +1,30 @@
+import cv2
+
+#-----Reading the image-----------------------------------------------------
+img = cv2.imread('Dog.jpg', 1)
+cv2.imshow("img",img) 
+
+#-----Converting image to LAB Color model----------------------------------- 
+lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+cv2.imshow("lab",lab)
+
+#-----Splitting the LAB image to different channels-------------------------
+l, a, b = cv2.split(lab)
+cv2.imshow('l_channel', l)
+cv2.imshow('a_channel', a)
+cv2.imshow('b_channel', b)
+
+#-----Applying CLAHE to L-channel-------------------------------------------
+clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+cl = clahe.apply(l)
+cv2.imshow('CLAHE output', cl)
+
+#-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+limg = cv2.merge((cl,a,b))
+cv2.imshow('limg', limg)
+
+#-----Converting image from LAB Color model to RGB model--------------------
+final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+cv2.imshow('final', final)
+
+#_____END_____#
