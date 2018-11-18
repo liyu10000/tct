e777b4c175fcc4359ac57752cd47b8d8b2111c37
liyu10000 <yuli00986081@gmail.com>
2018-11-18 16:51:41 +0800

    slight modification

--- conference/get_label.py
+++ conference/get_label.py
@@ -5,7 +5,8 @@ with open("chosen.pkl", 'rb') as f:
     chosen = pickle.load(f)
 
 def get_label(chosen, basename):
-    for key,value in chosen:
-        if key == basename:
-            return value["label"].split('+')  # list of labels
+    for class_i,entries in chosen.items():
+        for entry in entries:
+            if entry[0] == basename:
+                return entry[1]["label"].split('+')  # list of labels
     return None
