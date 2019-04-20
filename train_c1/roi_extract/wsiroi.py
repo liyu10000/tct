bb44d3b73371309c29d8de14f7893c60340c5966
yuli <yuli00986081@gmail.com>
2019-04-20 13:44:34 +0800

    updated roi algorithm on TMAP files

--- train_c1/roi_extract/wsiroi.py
+++ train_c1/roi_extract/wsiroi.py
@@ -1,8 +1,6 @@
 import cv2
 import numpy as np
-import openslide
 from skimage.filters import threshold_otsu
-
 from Aslide import aslide
 
 
@@ -14,17 +12,18 @@ class WSIROI:
 
 
     def find_roi(self):
-        try:
-            slide = openslide.OpenSlide(self.wsi_name)
-        except:
-            slide = aslide.Aslide(self.wsi_name)
+        slide = aslide.Aslide(self.wsi_name)
         m, n = slide.dimensions
         level = slide.level_count - 1
-        self.downsample = slide.level_downsamples[level]
-        
-        source = np.array(slide.read_region((0, 0), level, slide.level_dimensions[level]).convert('RGB'))
+        #self.downsample = slide.level_downsamples[level]
+        thumbnail = slide.read_region((0, 0), level, slide.level_dimensions[level]).convert('RGB')
         slide.close()
-        img_gray = cv2.cvtColor(source, cv2.COLOR_RGB2GRAY)
+
+        t_m, t_n = thumbnail.size
+        self.downsample = (m/t_m + n/t_n) / 2
+        self.thumbnail = np.array(thumbnail)
+        
+        img_gray = cv2.cvtColor(self.thumbnail, cv2.COLOR_RGB2GRAY)
         img_gray = cv2.pyrDown(img_gray)
         img_gray = cv2.pyrDown(img_gray)
         
@@ -43,6 +42,8 @@ class WSIROI:
         self.hull = cv2.convexHull(contours[1], False) * 4
         area = cv2.contourArea(self.hull)*self.downsample*self.downsample
         self.area_ratio = area/m/n
+        self.m = m
+        self.n = n
         
 
     def get_area_ratio(self):
@@ -51,6 +52,18 @@ class WSIROI:
 
     def get_roi(self):
         return self.hull  # np.ndarray, (num, 1, 2)
+
+
+    def get_thumbnail(self):
+        return self.thumbnail
+
+
+    def get_downsample(self):
+        return self.downsample
+    
+    
+    def get_dimensions(self):
+        return (self.m, self.n)
 
 
     def in_roi_simplified(self, x, y, w, h):
@@ -91,9 +104,18 @@ class WSIROI:
         return img_pool.astype(np.uint8)
 
 
+def plot(image, x, y, w, h, downsample):
+    x = int(x / downsample)
+    y = int(y / downsample)
+    w = int(w / downsample)
+    h = int(h / downsample)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255,0,0), 2)
+
+
 
 if __name__ == "__main__":
-    wsi_name = "./2017-10-11 17_45_42.tif"
+    wsi_name = "TC19016138.TMAP"
+    thumb_name = "./TC19016138.jpg"
 
     wsi_roi = WSIROI(wsi_name)
 
@@ -101,10 +123,23 @@ if __name__ == "__main__":
     hull = wsi_roi.get_roi()
     print(area_ratio, hull.shape)
     
-    m, n = 20000, 20000
+    downsample = wsi_roi.get_downsample()
+    m, n = wsi_roi.get_dimensions()
+    i_start, i_end = int(m * 0.05), int(m * 0.95)
+    j_start, j_end = int(n * 0.05), int(n * 0.95)
+    size = 1216
 
-    for i in range(0, m, 1216):
-        for j in range(0, n, 1216):
-            is_in1 = wsi_roi.in_roi_simplified(i, j, 1216, 1216)
-            is_in2 = wsi_roi.in_roi_completed(i, j, 1216, 1216)
-            print(is_in1, is_in2)
+    count = 0
+    thumbnail = wsi_roi.get_thumbnail()
+    thumbnail = cv2.cvtColor(thumbnail, cv2.COLOR_RGB2BGR)
+    for i in range(i_start, i_end, size):
+        for j in range(j_start, j_end, size):
+            is_in1 = wsi_roi.in_roi_simplified(i, j, size, size)
+            is_in2 = wsi_roi.in_roi_completed(i, j, size, size)
+            # print(i, j, is_in1, is_in2)
+            if is_in2:
+                count += 1
+                plot(thumbnail, i, j, size, size, downsample)
+    print(count)
+    
+    cv2.imwrite(thumb_name, thumbnail)
